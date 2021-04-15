5816bc0e32f4c9ba9a3f79e6edf4af489e16003d
ASEEL ALBANNA <ASEELNIHAD.AN@GMAIL.COM>
2021-04-15 20:42:55 +0300

    challenge 10 is done

---
+++ data-structure/stack-and-queue/app/src/main/java/stack/and/queue/Node.java
@@ -0,0 +1,17 @@
+package stack.and.queue;
+
+public class Node <T>{
+    T value;
+    Node next;
+    Node(T value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
+    }
+}
