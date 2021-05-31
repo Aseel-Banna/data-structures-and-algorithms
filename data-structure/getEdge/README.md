395d6a9e627bbb4cbb221afaf871648528d26c4f
ASEEL ALBANNA <ASEELNIHAD.AN@GMAIL.COM>
2021-05-31 22:49:00 +0300

    challenge 38 is done

---
+++ data-structure/getEdge/README.md
@@ -0,0 +1,9 @@
+# Get Edge
+
+## Challenge 
+Write a function which takes in a graph, and an array of city names and return whether the full trip is possible with direct flights, and how much it would cost.
+
+## Approach and Efficiency
+The Time complexity for this method is O(n * m) where m represents the degree of the graph. Where the space complexity is O(1) since we just return the cost.
+
+![whiteboard](ww3.png)
---
+++ data-structure/getEdge/app/src/main/java/getEdge/Edge.java
@@ -0,0 +1,21 @@
+package getEdge;
+
+public class Edge<T> {
+    public Node<T> neighbor;
+    public int weight;
+
+    public Edge(Node<T> neighbor) {
+        this.neighbor = neighbor;
+        this.weight = 0;
+    }
+
+    public Edge(Node<T> neighbor, int weight) {
+        this.neighbor = neighbor;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Node:" + this.neighbor + " Edge Weight:" + this.weight;
+    }
+}
